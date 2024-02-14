0caa1eb16681ee83683bc6dc176f3286cd2db5ee
sarthakgit21 <110364481+sarthakgit21@users.noreply.github.com>
2024-02-14 19:09:00 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,6 +1,6 @@
 import mongoose  from 'mongoose';
 
-const Connection = async (username = 'usercode', password = 'codeforinterview') => {
+const Connection = async (username = '', password = '') => {
     const URL = `mongodb://${username}:${password}@google-docs-clone-shard-00-00.u7w63.mongodb.net:27017,google-docs-clone-shard-00-01.u7w63.mongodb.net:27017,google-docs-clone-shard-00-02.u7w63.mongodb.net:27017/GOOGLE-DOCS?ssl=true&replicaSet=atlas-zfichu-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
     try {
